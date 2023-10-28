d19e199e72a4da48c35cef66b8759e01630f14f4
Jacob <weaver.jacob.t@gmail.com>
2023-10-28 18:48:37 -0500

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const submit = document.getElementById('submit');
+  submit.addEventListener('click', (event) => {
+    event.preventDefault();
+    //grab elements
+    let newTaskDes = document.getElementById("new-task-description").value;
+    console.log(newTaskDes);
+    let tasks = document.getElementById('tasks');
+    
+    // create new li and set value to text contents of newTaskDes
+    newTask = tasks.appendChild(document.createElement("li"));
+    newTask.textContent = newTaskDes;
+   
+    
+    
+  })
+
+
+
 });
